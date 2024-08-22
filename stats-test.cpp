5f0ebaa81d9c84d53be04cd45b8391f6b8dc15a9
sahanad <sahana.d@philips.com>
2024-08-22 11:02:43 +0530

    Initial commit

--- stats-test.cpp
+++ stats-test.cpp
@@ -13,6 +13,9 @@ TEST(Statistics, ReportsAverageMinMax) {
 
 TEST(Statistics, AverageNaNForEmpty) {
     auto computedStats = Statistics::ComputeStatistics({});
+    EXPECT_TRUE(std::isnan(computedStats.average));
+    EXPECT_TRUE(std::isnan(computedStats.max));
+    EXPECT_TRUE(std::isnan(computedStats.min));
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
